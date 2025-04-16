9608e43d8b2eff5cb182c83680b8022f3b43cfde
Soha18 <sohasaleh@msn.com>
2025-04-16 16:24:50 -0600

    final project proposal

--- build.sh
+++ build.sh
@@ -1,37 +1,48 @@
 #!/bin/bash
-# Script to build image for qemu.
-# Author: Siddhant Jajoo.
+# Script to build image for Raspberry Pi 3
+# Author: Soha (adapted from Siddhant Jajoo)
 
+set -e
+
+# Initialize and sync submodules (if used)
 git submodule init
 git submodule sync
 git submodule update
 
-# local.conf won't exist until this step on first execution
+# Source the environment and create build directory
 source poky/oe-init-build-env
 
-CONFLINE="MACHINE = \"qemuarm64\""
+# Set target MACHINE
+MACHINE_LINE='MACHINE = "raspberrypi3-64"'
 
-cat conf/local.conf | grep "${CONFLINE}" > /dev/null
-local_conf_info=$?
-
-if [ $local_conf_info -ne 0 ];then
-	echo "Append ${CONFLINE} in the local.conf file"
-	echo ${CONFLINE} >> conf/local.conf
-	
+# Append MACHINE setting if not already present
+if ! grep -q "${MACHINE_LINE}" conf/local.conf; then
+    echo "Appending ${MACHINE_LINE} to conf/local.conf"
+    echo "${MACHINE_LINE}" >> conf/local.conf
 else
-	echo "${CONFLINE} already exists in the local.conf file"
+    echo "${MACHINE_LINE} already exists in conf/local.conf"
 fi
 
-
-bitbake-layers show-layers | grep "meta-aesd" > /dev/null
-layer_info=$?
-
-if [ $layer_info -ne 0 ];then
-	echo "Adding meta-aesd layer"
-	bitbake-layers add-layer ../meta-aesd
+# Add meta-raspberrypi layer
+if ! bitbake-layers show-layers | grep -q "meta-raspberrypi"; then
+    echo "Adding meta-raspberrypi layer"
+    bitbake-layers add-layer ../meta-raspberrypi
 else
-	echo "meta-aesd layer already exists"
+    echo "meta-raspberrypi layer already added"
 fi
 
-set -e
-bitbake core-image-aesd
+# Add meta-openembedded sublayers (individually)
+OPENEMBEDDED_LAYERS=("meta-oe" "meta-python" "meta-networking")
+
+for layer in "${OPENEMBEDDED_LAYERS[@]}"; do
+    if ! bitbake-layers show-layers | grep -q "$layer"; then
+        echo "Adding $layer layer from meta-openembedded"
+        bitbake-layers add-layer ../meta-openembedded/$layer
+    else
+        echo "$layer layer already added"
+    fi
+done
+
+# Build the image (replace with your custom image if needed)
+bitbake core-image-minimal
+
